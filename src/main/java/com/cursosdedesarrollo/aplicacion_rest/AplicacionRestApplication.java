6c0f44797ba74f9ae5f82c1dece4a9af39e25e51
Sergio Blanco <noacuerdo@algo.com>
2024-11-05 20:38:47 -0500

    primer commit

---
+++ src/main/java/com/cursosdedesarrollo/aplicacion_rest/AplicacionRestApplication.java
@@ -0,0 +1,13 @@
+package com.cursosdedesarrollo.aplicacion_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionRestApplication.class, args);
+	}
+
+}
